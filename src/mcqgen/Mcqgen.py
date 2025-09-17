7dc94f2b24bffe7aa3f522bff912af9ecc33bcbc
Rajlxi29 <nileshraji811001@gmail.com>
2025-09-17 17:15:59 +0530

    added chains

--- src/mcqgen/Mcqgen.py
+++ src/mcqgen/Mcqgen.py
@@ -22,3 +22,44 @@ llm = ChatOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
     base_url="https://openrouter.ai/api/v1"
 )
+
+quiz_template = """
+Text:{text}
+You are an expert MCQ maker. Given the above text, it is your job to \
+create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
+Make sure the questions are not repeated and check all the questions to be conforming the text as well.
+Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
+Ensure to make {number} MCQs
+### RESPONSE_JSON
+{response_json}
+
+"""
+
+quiz_prompt = PromptTemplate(
+    input_variables=["text","number","subject","tone","response_json"],
+    template=quiz_template
+)
+
+quiz_chain = LLMChain(llm=llm, prompt=quiz_prompt, output_key="quiz", verbose=True)
+
+evaluate_template="""
+You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
+You need to evaluate the complexity of the question and give a complete analysis of the quiz. Only use at max 50 words for complexity analysis. 
+if the quiz is not at per with the cognitive and analytical abilities of the students,\
+update the quiz questions which needs to be changed and change the tone such that it perfectly fits the student abilities
+Quiz_MCQs:
+{quiz}
+
+Check from an expert English Writer of the above quiz:"""
+
+evaluate_prompt = PromptTemplate(
+    input_variables = ["subject", "quiz"],
+    template = evaluate_template
+)
+
+eval_chain = LLMChain(llm=llm, prompt=evaluate_prompt, output_key="review", verbose=True)
+
+final_mcq_generator = SequentialChain(chains=[quiz_chain, eval_chain],
+                                      input_variables = ["text", "number", "subject", "tone", "response_json"],
+                                      output_variables = ["quiz", "review"], verbose = True)
+
